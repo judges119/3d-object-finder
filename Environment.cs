58dc1484a33145571bacd5ea60d2275e57cdb856
judges119 <adam.ogrady@gmail.com>
2013-12-22 17:17:13 +0800

    updated files

--- Environment.cs
+++ Environment.cs
@@ -6,6 +6,7 @@ public class Environment : MonoBehaviour {
 	//Private variables
 	GameObject[] enviroObjects = new GameObject[50]; //Extra objects in scene
 	GameObject targetObject; //Target object in scene
+	GameObject targetCamera;
 	bool finished = false; //Finished game boolean
 
 	//For GUI events
@@ -26,11 +27,15 @@ public class Environment : MonoBehaviour {
 		tempObject = GameObject.CreatePrimitive(PrimitiveType.Sphere); //Sphere type for target object
 		targetObject = Instantiate(tempObject) as GameObject; //Create a sphere for the target object
 		targetObject.transform.position = new Vector3(Random.value * 100 - 50, Random.value * 100 - 50, Random.value * 100 - 50); //Set location of target object sphere
+		GameObject.Destroy(tempObject);
 		tempObject = GameObject.CreatePrimitive(PrimitiveType.Cube); //Cube type for target object
 		for (int i = 0; i < 50; i++) {
 			enviroObjects[i] = Instantiate(tempObject) as GameObject; //Create cube objects for each extra object
 			enviroObjects[i].transform.position = new Vector3(Random.value * 100 - 50, Random.value * 100 - 50, Random.value * 100 - 50); //Set locations of extra objects
+			enviroObjects[i].transform.eulerAngles = new Vector3(Random.value * (360 - 1), Random.value * (360 - 1), Random.value * (360 - 1)); //Rotate cubes randomly
 		}
+		GameObject.Destroy(tempObject);
+		targetCamera.AddComponent<Camera>();
 	}
 	
 	// Update is called once per frame
--- LeapMovement.cs
+++ LeapMovement.cs
@@ -36,8 +36,8 @@ public class LeapMovement : MonoBehaviour {
 							gameObject.transform.Rotate(Vector3.forward * -1, h.handUpd.RotationAngle(LeapConnect2.lastFrame) * 75); //Rotate around the backward (z-axis local to main camera) axis by the angle of the hand's rotation (compared to last frame)
 						}
 						if (h.handUpd.TranslationProbability(LeapConnect2.lastFrame) > 0.5) { //If probability of translation is greater than 50%...
-							gameObject.transform.Rotate(Vector3.up, h.handUpd.Translation(LeapConnect2.lastFrame).x / 2); //Horizontal translation equals y-axis rotation
-							gameObject.transform.Rotate(Vector3.left, h.handUpd.Translation(LeapConnect2.lastFrame).y / 2); //Vertical translation equals x-axis rotation
+							gameObject.transform.Rotate(Vector3.up, h.handUpd.Translation(LeapConnect2.lastFrame).x); //Horizontal translation equals y-axis rotation
+							gameObject.transform.Rotate(Vector3.left, h.handUpd.Translation(LeapConnect2.lastFrame).y); //Vertical translation equals x-axis rotation
 						}
 					}
 				}
@@ -45,8 +45,19 @@ public class LeapMovement : MonoBehaviour {
 		}
 	}
 	
-	//Move camera based on instance/distance of two open hands
+	//Moves camera forward/backward by doing a "zooming" motion with hands, that is two open hands moved apart or closer togetheru
 	void translateCamera() {
+		if (LeapConnect2.m_Frame != null) {
+			if ((LeapConnect2.m_Frame.Hands.Count >= 2) && (LeapConnect2.m_Frame.Fingers.Count >= 4)) {
+				if (LeapConnect2.m_Frame.ScaleFactor(LeapConnect2.lastFrame) > 0.5) {
+					transform.Translate(Vector3.forward * (LeapConnect2.m_Frame.ScaleFactor(LeapConnect2.lastFrame) - 1) * 50);
+				}
+			}
+		}
+	}
+
+	//This is the old method that sped you forward when two open hands were held over the controller (was supposed to be based on their distance apart, but never worked on perfecting it to that)
+	/*void translateCamera() {
 		float[] closestZHands = new float[3] {-1, -1, 10}; //Array to hold pair of hands with smallest z-axis difference, difference set higher than possible normalised values
 		for (int i = 0; i < LeapConnect2.hands.Length; i++) { //Compare each hand
 			for (int j = i + 1; j < LeapConnect2.hands.Length; j++) { //Against each other hand in the LeapConnect2.hands array, without doubling up on comparisons
@@ -67,5 +78,5 @@ public class LeapMovement : MonoBehaviour {
 			float distance = firstHand.DistanceTo(secondHand); //Distance between both vectors
 			gameObject.transform.Translate(Vector3.forward * distance / 5); //Move camera forward by distance
 		}
-	}
+	}*/
 }
